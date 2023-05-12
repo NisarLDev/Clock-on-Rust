6f48d34681166688e4c09f84cffa1ddfc03a8466
NisarLDev <HarusBadah@proton.me>
2023-05-12 05:28:01 +0200

    Finished Programme

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use chrono::{Local};
-use chrono::DateTime;
-const _DIGITS : [[&str; 11]; 7] = [
+const DIGITS : [[&str; 11]; 7] = [
     ["┏━┓ ","  ╻  "," ┏━┓ ", " ┏━┓ "," ╻ ╻ "," ┏━┓ "," ┏   "," ┏━┓ "," ┏━┓ "," ┏━┓ ","   "],
     ["┃ ┃ ","  ┃  ","   ┃ ", "   ┃ "," ┃ ┃ "," ┃   "," ┃   ","   ┃ "," ┃ ┃ "," ┃ ┃ "," ╻ "],
     ["┃ ┃ ","  ┃  ","   ┃ ", "   ┃ "," ┃ ┃ "," ┃   "," ┃   ","   ┃ "," ┃ ┃ "," ┃ ┃ ","   "],
@@ -11,6 +10,25 @@ const _DIGITS : [[&str; 11]; 7] = [
 ];
 
 fn main() {
-    let t: DateTime<Local> = Local::now();	
-    println!("{:?}", t);
+    print!("\x1b[2J");
+    print!("\x1b[?25l");
+    loop {
+        let t = Local::now();
+        let time = t.format("%H:%M:%S").to_string();
+        for row in &DIGITS {
+            for c in time.chars() {
+                let col = match c {
+                    '0'..='9' => c as usize - '0' as usize,
+                    _ => 10,
+                };
+                print!("{} ", row[col]);
+            }
+            println!();
+        }
+        std::thread::sleep(std::time::Duration::from_millis(999));
+        print!("\x1b[7A");
+   }
 }
+
+// END OF CODE
+// END OF FILE
